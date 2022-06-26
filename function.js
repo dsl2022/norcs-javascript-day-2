f32a63421b9ddbb879b45851612c8bef367fb028
dsl2055 <job2019tmm@gmail.com>
2022-06-26 14:32:37 -0400

    updated

--- function.js
+++ function.js
@@ -12,3 +12,34 @@ function add(num1, num2) {
 
 const result = add(3, 4);
 console.log({ result });
+
+let user = {
+  firstName: "Eli",
+  lastName: "Hale",
+};
+
+function fullName({ firstName, lastName }) {
+  console.log(firstName + " " + lastName);
+}
+
+fullName(user);
+// ?
+
+function add(a, b) {
+  console.log(a + b);
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+const house = "broken and old and ugly";
+
+function buildHouse(house) {
+  house = "pretty, fancy and functional";
+  return house;
+}
+
+const result3 = subtract(add(1, 2), 1);
+console.log(result3);
