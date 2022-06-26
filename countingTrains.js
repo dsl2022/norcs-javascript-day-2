a2640e0d9bf76c4f082495aa2becc735a4af2b74
dsl2055 <job2019tmm@gmail.com>
2022-06-26 11:40:22 -0400

    updated

--- countingTrains.js
+++ countingTrains.js
@@ -1,4 +1,4 @@
-let newInventoryCarModels = ["S1", "T1", "S2", "S1", "S2", "S1"];
+let newInventoryCarModels = ["S1", "S1", "S2", "S1", "S2", "S1"];
 let newInventoryCarTypes = [
   "StockCar",
   "Refrigerator",
@@ -9,13 +9,26 @@ let newInventoryCarTypes = [
 ];
 let newInventoryYearBuilt = [1922, 2011, 1975, 2002, 1965, 1951];
 
-let i = 0;
+// let i = 0;
 let typeS1Count = 0;
-while (i < newInventoryCarModels.length) {
+// while (i < newInventoryCarModels.length) {
+//   if (newInventoryCarModels[i] === "S1") {
+//     typeS1Count += 1;
+//   }
+//   i += 1;
+// }
+
+for (let i = 0; i < newInventoryCarModels.length; i++) {
   if (newInventoryCarModels[i] === "S1") {
     typeS1Count += 1;
   }
-  i += 1;
 }
 
-console.log({ typeS1Count });
+let currentYear = 2022;
+let total = 0;
+for (let i = 0; i < newInventoryYearBuilt.length; i++) {
+  const ageOfCar = currentYear - newInventoryYearBuilt[i];
+  total += ageOfCar;
+}
+const average = total / newInventoryYearBuilt.length;
+console.log({ average });
